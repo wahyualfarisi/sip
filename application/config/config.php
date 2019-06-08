e5164ecc499ca15dc1b48773fbe6982c2ee26b60
wahyu alfarisi <wahyualfarisi@gmail.com>
2019-06-08 23:12:43 +0700

    add

--- application/config/config.php
+++ application/config/config.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "http" : "http");
+$config['base_url'] = ((isset($_SERVER['HTTP']) && $_SERVER['HTTP'] == "on") ? "http" : "http");
 $config['base_url'] .= "://" .$_SERVER['HTTP_HOST'];
 $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 
